1d7b156393e062fd37fad9369301c86c4647061f
amandeepmittal <amandeepmittal@live.com>
2017-06-18 15:37:46 +0530

    add a modal

--- 038-quotes-app/src/pages/favorites/favorites.ts
+++ 038-quotes-app/src/pages/favorites/favorites.ts
@@ -1,7 +1,8 @@
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ModalController, NavController, NavParams } from 'ionic-angular';
 
 import { Component } from '@angular/core';
 import { Quote } from '../../data/quote.interface';
+import { QuotePage } from '../quote/quote';
 import { QuotesProvider } from '../../providers/quotes/quotes';
 
 @IonicPage()
@@ -11,11 +12,16 @@ import { QuotesProvider } from '../../providers/quotes/quotes';
 })
 export class FavoritesPage {
   quotes: Quote[];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public quotesService: QuotesProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public quotesService: QuotesProvider, public modalCtrl: ModalController) {
   }
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create(QuotePage);
+    modal.present();
   }
 
 }
