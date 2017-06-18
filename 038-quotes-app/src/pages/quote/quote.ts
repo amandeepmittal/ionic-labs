7a4f3f6fd185a61abcc4f8a524a3aa3eb3a1da9c
amandeepmittal <amandeepmittal@live.com>
2017-06-18 15:47:09 +0530

    passing data to a modal using navParams

--- 038-quotes-app/src/pages/quote/quote.ts
+++ 038-quotes-app/src/pages/quote/quote.ts
@@ -8,9 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+  person: string;
+  text: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
   }
+
+  ionViewDidLoad() {
+    this.person = this.navParams.get('person');
+    this.text = this.navParams.get('text');
+  }
+
   onClose() {
     this.viewCtrl.dismiss();
   }
