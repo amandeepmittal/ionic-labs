9673962a50e176986ce869fecdd0a588426e8d0b
amandeepmittal <amandeepmittal@live.com>
2017-06-18 10:59:05 +0530

    using navigation directives

---
+++ 029-naviagtion-directives/README.md
@@ -0,0 +1,4 @@
+# Push and Pop Pages using Navigation Directives
+* `[navPush]`
+* `[navParams]`
+* navPop
---
+++ 029-naviagtion-directives/src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ShopPage } from '../shop/shop';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  shopPage = ShopPage;
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
