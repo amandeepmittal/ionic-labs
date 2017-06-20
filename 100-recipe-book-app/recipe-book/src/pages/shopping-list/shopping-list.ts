bf0001b3de427e1121bc5e29c0713329305d6f51
amandeepmittal <amandeepmittal@live.com>
2017-06-20 18:54:18 +0530

    displaying and removing items from shopping list

--- 100-recipe-book-app/recipe-book/src/pages/shopping-list/shopping-list.ts
+++ 100-recipe-book-app/recipe-book/src/pages/shopping-list/shopping-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NgForm } from "@angular/forms";
 import { ShoppingListProvider } from "../../providers/shopping-list/shopping-list";
+import { Ingredient } from "../../models/ingredients";
 
 @IonicPage()
 @Component({
@@ -9,12 +10,26 @@ import { ShoppingListProvider } from "../../providers/shopping-list/shopping-lis
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
-
+  shoppingListItems: Ingredient[];
   constructor(public navCtrl: NavController, public navParams: NavParams, public shoppingListService: ShoppingListProvider){
   }
 
   onAddItem(form: NgForm) {
     this.shoppingListService.addItem(form.value.ingredientName, form.value.amount);
     form.reset();
+    this.loadItems();
+  }
+
+  ionViewWillEnter() {
+    this.loadItems();
+  }
+
+  onCheckItem(index: number) {
+    this.shoppingListService.removeItem(index);
+    this.loadItems();
+  }
+
+  private loadItems() {
+    this.shoppingListItems = this.shoppingListService.getItems();
   }
 }
