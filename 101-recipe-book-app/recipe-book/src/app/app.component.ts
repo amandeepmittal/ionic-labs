18af6c8936b1838b4a72383c88189a83650e5ea0
amandeepmittal <amandeepmittal@live.com>
2017-06-22 14:47:46 +0530

    setup firebase

--- 101-recipe-book-app/recipe-book/src/app/app.component.ts
+++ 101-recipe-book-app/recipe-book/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import * as firebase from 'firebase';
+
 import { Component, ViewChild } from '@angular/core';
 import { MenuController, NavController, Platform } from 'ionic-angular';
 
@@ -17,6 +19,12 @@ export class MyApp {
   @ViewChild('nav') nav: NavController;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public menuCtrl: MenuController) {
+    // firebase setup
+    firebase.initializeApp({
+      apiKey: "AIzaSyAPAj3pgUhNBMu7a8SYufk1SQaou03Aj4U",
+    authDomain: "ionic3-recipe-book-79d1f.firebaseapp.com"
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
